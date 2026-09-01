ffad862f5356dba120a74348f2b14ae663bae927
sdarwin <samuel.d.darwin@gmail.com>
2026-09-01 20:01:02 +0000

    2026-09 images

--- scripts/prod_amis.sh
+++ scripts/prod_amis.sh
@@ -8,17 +8,19 @@ set -xe
 declare -A all_amis
 
 # all_amis - keep this line
-all_amis[ubuntu-jammy.yaml]=github-runner-ubuntu-jammy-amd64-202605121607
-all_amis[ubuntu-focal-arm64.yaml]=github-runner-ubuntu-focal-arm64-202605121607
-all_amis[ubuntu-jammy-arm64.yaml]=github-runner-ubuntu-jammy-arm64-202605121607
+all_amis[ubuntu-resolute.yaml]=github-runner-ubuntu-resolute-amd64-202609011632
+all_amis[ubuntu-resolute-arm64.yaml]=github-runner-ubuntu-resolute-arm64-202609011632
+all_amis[ubuntu-jammy.yaml]=github-runner-ubuntu-jammy-amd64-202609011756
+all_amis[ubuntu-focal-arm64.yaml]=github-runner-ubuntu-focal-arm64-202609011756
+all_amis[ubuntu-jammy-arm64.yaml]=github-runner-ubuntu-jammy-arm64-202609011756
 all_amis[ubuntu-bionic.yaml]=github-runner-ubuntu-bionic-amd64-202411261305
-all_amis[ubuntu-focal.yaml]=github-runner-ubuntu-focal-amd64-202605121607
+all_amis[ubuntu-focal.yaml]=github-runner-ubuntu-focal-amd64-202609011756
 all_amis[ubuntu-bionic-arm64.yaml]=github-runner-ubuntu-bionic-arm64-202410151633
-all_amis[ubuntu-noble.yaml]=github-runner-ubuntu-noble-amd64-202605121607
-all_amis[ubuntu-noble-arm64.yaml]=github-runner-ubuntu-noble-arm64-202605121607
-all_amis[windows-2019.yaml]=github-runner-windows-2019-amd64-202605121607
-all_amis[windows-2022.yaml]=github-runner-windows-2022-amd64-202605121607
-all_amis[windows-2025.yaml]=github-runner-windows-2025-amd64-202605121607
+all_amis[ubuntu-noble.yaml]=github-runner-ubuntu-noble-amd64-202609011756
+all_amis[ubuntu-noble-arm64.yaml]=github-runner-ubuntu-noble-arm64-202609011756
+all_amis[windows-2019.yaml]=github-runner-windows-2019-amd64-202609011756
+all_amis[windows-2022.yaml]=github-runner-windows-2022-amd64-202609011756
+all_amis[windows-2025.yaml]=github-runner-windows-2025-amd64-202609011756
 
 # Update the templates with the above values
 
