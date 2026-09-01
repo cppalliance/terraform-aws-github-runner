8476035cacbd9587e7535afa9517b3896327af5c
sdarwin <samuel.d.darwin@gmail.com>
2026-09-01 15:53:24 +0000

    2026-05-12 image rebuild, commit out of order

--- scripts/prod_amis.sh
+++ scripts/prod_amis.sh
@@ -8,19 +8,17 @@ set -xe
 declare -A all_amis
 
 # all_amis - keep this line
-all_amis[ubuntu-jammy.yaml]=github-runner-ubuntu-jammy-amd64-202601211431
-all_amis[ubuntu-focal-arm64.yaml]=github-runner-ubuntu-focal-arm64-202601211431
-all_amis[ubuntu-jammy-arm64.yaml]=github-runner-ubuntu-jammy-arm64-202601211431
+all_amis[ubuntu-jammy.yaml]=github-runner-ubuntu-jammy-amd64-202605121607
+all_amis[ubuntu-focal-arm64.yaml]=github-runner-ubuntu-focal-arm64-202605121607
+all_amis[ubuntu-jammy-arm64.yaml]=github-runner-ubuntu-jammy-arm64-202605121607
 all_amis[ubuntu-bionic.yaml]=github-runner-ubuntu-bionic-amd64-202411261305
-all_amis[ubuntu-focal.yaml]=github-runner-ubuntu-focal-amd64-202601211431
+all_amis[ubuntu-focal.yaml]=github-runner-ubuntu-focal-amd64-202605121607
 all_amis[ubuntu-bionic-arm64.yaml]=github-runner-ubuntu-bionic-arm64-202410151633
-all_amis[ubuntu-noble.yaml]=github-runner-ubuntu-noble-amd64-202601211431
-all_amis[ubuntu-noble-arm64.yaml]=github-runner-ubuntu-noble-arm64-202601211431
-all_amis[ubuntu-resolute-arm64.yaml]=github-runner-ubuntu-resolute-arm64-__
-all_amis[ubuntu-resolute.yaml]=github-runner-ubuntu-resolute-amd64-__
-all_amis[windows-2019.yaml]=github-runner-windows-2019-amd64-202601211541
-all_amis[windows-2022.yaml]=github-runner-windows-2022-amd64-202601211541
-all_amis[windows-2025.yaml]=github-runner-windows-2025-amd64-202601211541
+all_amis[ubuntu-noble.yaml]=github-runner-ubuntu-noble-amd64-202605121607
+all_amis[ubuntu-noble-arm64.yaml]=github-runner-ubuntu-noble-arm64-202605121607
+all_amis[windows-2019.yaml]=github-runner-windows-2019-amd64-202605121607
+all_amis[windows-2022.yaml]=github-runner-windows-2022-amd64-202605121607
+all_amis[windows-2025.yaml]=github-runner-windows-2025-amd64-202605121607
 
 # Update the templates with the above values
 
